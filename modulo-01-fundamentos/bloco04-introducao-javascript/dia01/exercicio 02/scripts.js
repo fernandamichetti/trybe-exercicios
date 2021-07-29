4377fde6b17e16e6dc43c38a8788c58a0d725a0d
fernandamichetti <f.michetti@hotmail.com>
2021-07-29 12:01:09 -0300

    Exercício 2, sobre if e else

--- modulo-01-fundamentos/bloco04-introducao-javascript/dia01/exercicio 02/scripts.js
+++ modulo-01-fundamentos/bloco04-introducao-javascript/dia01/exercicio 02/scripts.js
@@ -6,3 +6,12 @@ console.log('Subtração: ' + (a - b))
 console.log('Multiplicação: ' + a * b)
 console.log('Divisão: ' + a / b)
 console.log('Módulo: ' + (a % b))
+/*Exercício 2*/
+let c = 60
+let d = 40
+
+if (c > d) {
+  console.log(c)
+} else {
+  console.log(d)
+}
