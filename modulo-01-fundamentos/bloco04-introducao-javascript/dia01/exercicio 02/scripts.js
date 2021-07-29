d7a86a4b40e85d365bf13dba4e36b37fcf34b904
fernandamichetti <f.michetti@hotmail.com>
2021-07-29 12:00:37 -0300

    Exercício 1, sobre operaçòes aritméticas

---
+++ modulo-01-fundamentos/bloco04-introducao-javascript/dia01/exercicio 02/scripts.js
@@ -0,0 +1,8 @@
+let a = 28
+let b = 10
+
+console.log('Adição: ' + (a + b))
+console.log('Subtração: ' + (a - b))
+console.log('Multiplicação: ' + a * b)
+console.log('Divisão: ' + a / b)
+console.log('Módulo: ' + (a % b))
