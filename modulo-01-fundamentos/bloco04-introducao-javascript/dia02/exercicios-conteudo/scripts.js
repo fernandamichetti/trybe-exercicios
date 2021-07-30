ffb1651f3e63c936547fd3599c339bfcad12b7ba
fernandamichetti <f.michetti@hotmail.com>
2021-07-30 15:12:51 -0300

    Exercício sobre for/of

--- modulo-01-fundamentos/bloco04-introducao-javascript/dia02/exercicios-conteudo/scripts.js
+++ modulo-01-fundamentos/bloco04-introducao-javascript/dia02/exercicios-conteudo/scripts.js
@@ -23,3 +23,8 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia']
 for (let = index = 0; index < groceryList.length; index += 1) {
   console.log(groceryList[index])
 }
+/*Exercício 04 - For/of Utilize o for/of para imprimir os elementos da lista names com o console.log()*/
+let names = ['João', 'Maria', 'Antônio', 'Margarida']
+for (let lista of names) {
+  console.log(lista)
+}
