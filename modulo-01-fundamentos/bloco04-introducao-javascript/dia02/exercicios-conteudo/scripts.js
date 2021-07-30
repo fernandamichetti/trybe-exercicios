4f69d787ecf5071a6b881ca3816688a813f32468
fernandamichetti <f.michetti@hotmail.com>
2021-07-30 13:51:49 -0300

    Exercícios sobre Arrays

---
+++ modulo-01-fundamentos/bloco04-introducao-javascript/dia02/exercicios-conteudo/scripts.js
@@ -0,0 +1,18 @@
+/*Exercício 01
+Obtenha o valor "Serviços" do array menu*/
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links']
+let menuServices = menu[1]
+
+console.log(menuServices)
+
+/*Exercício 02
+Procure o índice do valor "Portfólio" do array menu*/
+
+let indexOfPortfolio = menu.indexOf('Portfólio')
+
+console.log(indexOfPortfolio)
+
+/*Exercício 03
+Adicione o valor "Contato" no final do array menu */
+menu.push('Contato')
+console.log(menu)
