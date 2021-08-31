5f7873183215c951d3fea7e568e66d245f977520
fernandamichetti <f.michetti@hotmail.com>
2021-08-31 12:08:53 -0300

    Segunda e ultima parte do primeiro exercício, do primeiro dia do bloco 7

--- modulo-01-fundamentos/bloco07-ES6-Testes-Unitarios/dia01-variables-arrow-functions-template-literals/script.js
+++ modulo-01-fundamentos/bloco07-ES6-Testes-Unitarios/dia01-variables-arrow-functions-template-literals/script.js
@@ -1,3 +1,5 @@
+//Parte 1
+
 testingScope = escopo => {
 if (escopo === true) {
   let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,3 +12,12 @@ if (escopo === true) {
 }
 
 testingScope(true)
+
+//Parte 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortedOddsAndEvens = oddsAndEvens.sort(function(a,b) {
+  return a - b;
+}); 
+
+console.log(`Os números ${sortedOddsAndEvens} se encontram ordenados de form crescente!`);
