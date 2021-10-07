92047e2dd7ff053471f23d768d2872397493561b
fernandamichetti <f.michetti@hotmail.com>
2021-10-07 19:57:53 -0300

    Exercício de fixação do conteúdo 11.1

---
+++ modulo-02-front-end/bloco11-introducao-react/dia01-hello-world/hello-world-react/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './App.css';
+import Hello from './Component'
+
+function App() {
+  return <Hello/>
+}
+
+export default App;
---
+++ modulo-02-front-end/bloco11-introducao-react/dia01-hello-world/hello-world-react/src/Component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Hello extends React.Component{
+  render() {
+    return <div><h1>Fernanda Michetti</h1>
+    <p>Eu sou a Fernanda, tenho 24 anos, estudo Desenvolvimento Web na Trybe e trabalho como desenvolvedora front end</p>
+    </div>
+  }
+}
+
+export default Hello;
